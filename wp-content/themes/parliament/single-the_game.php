e747ed3a685c71cb34def51a2f2b4dc26fd61faf
timborovkov <tim.borovkov@icloud.com>
2022-11-26 20:44:08 +0200

    Move more stuff to components

--- wp-content/themes/parliament/single-the_game.php
+++ wp-content/themes/parliament/single-the_game.php
@@ -104,20 +104,8 @@
     require_once('game/tabs/my-party.php');
     require_once('game/tabs/legislation.php');
     require_once('game/tabs/taxes.php');
-    ?>
 
-    <div class="container">
-        <div class="personal_data jumbotron">
-            <h5><b><?= $player_name ?></b>, <?= $player_party ?></h5>
-            <h4>Turn: <b class="turn_number"><?= $turn ?></b></h4>
-            <p>1 year = 5 turns</p>
-            <h4>Actions left this turn: <b><?= $actions_left_this_turn ?></b></h4>
-            <h4>Country happiness: <b><?= $happiness ?></b></h4>
-            <h4>Turns to election: <b><?= $turns_until_election ?></b></h4>
-        </div>
-    </div>
-
-    <?php
+    require_once('game/jumbotron.php');
     require('game/next-turn.php');
     ?>
 
